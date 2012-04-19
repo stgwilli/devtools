e8657495c4a995f732ff0a8e886e4a2895030005
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-04-18 18:54:48 -0600

    Updated all of the tools to work with cygwin and bash

--- expansions/cygwin.rb
+++ expansions/cygwin.rb
@@ -7,6 +7,11 @@ expand do
     contents %w[shared hosts].as_glob_pattern
   end
 
+  shared_contents = %w[zsh rvm].map{|item| ['shared','dotfiles',item].as_glob_pattern}
+
+  copy_to configatron.core.home do
+    all_contents_in shared_contents
+  end
 
 
   partials = glob(%w[windows autohotkey partial_*.ahk].as_glob_pattern)
